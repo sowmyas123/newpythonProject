dc2935c76c5dfd98b85a3fbb0dd6977bd119e861
sowmyas123 <sowmyasiddu@gmail.com>
2024-02-07 16:18:25 +0530

    hello

---
+++ 02_02.Feb.2024/Lab009.py
@@ -0,0 +1,17 @@
+x=int(input("Enter the number to find table: "))
+for y in range(1,11):
+print(x,"*", y,"=",x*y)
+# print(f"{x} * {y} = {x*y}")
+
+# OR
+num = 2
+print(f"{num}*1 ={num}")
+print(f"{num}*2 = {num * 2}")
+print(f"{num}*3 = {num * 3}")
+print(f"{num}*4 = {num * 4}")
+print(f"{num}*5 = {num * 5}")
+print(f"{num}*6 = {num * 6}")
+print(f"{num}x7 = {num * 7}")
+print(f"{num}x8 = {num * 8}")
+print(f"{num}x9 = {num * 9}")
+print(f"{num}x10 = {num * 10}")
