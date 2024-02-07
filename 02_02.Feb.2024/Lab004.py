4c836a92969242367b7dfa1dfe1781d5f9d5ab86
sowmyas123 <sowmyasiddu@gmail.com>
2024-02-07 16:14:06 +0530

    hello

---
+++ 02_02.Feb.2024/Lab004.py
@@ -0,0 +1,25 @@
+print("Sowmya")
+print("sowmya", "BS", end="$")
+print("sowmya", "BS", "Bangalore", sep="_")
+#click on print to see builtins
+#def print(self, *args, sep=' ', end='\n', file=None): # known special case of print
+  #Prints the values to a stream, or to sys.stdout by default.
+  #*args - any no of arguments
+  #sep - space
+  #end - new line
+  #* - wild character i.e any no of characters, string, arguments
+  #argument - is what we put into the function
+      #sep
+       # string inserted between values, default a space.
+      #end
+       # string appended after the last value, default a newline.
+      #file
+       # a file-like object (stream); defaults to the current sys.stdout.
+      #flush
+     #   whether to forcibly flush the stream.
+#print function can also take multiple arguments
+
+result = max(3,4)
+print(result)
+
+#to see builtin functions google prebuilt python documentation
