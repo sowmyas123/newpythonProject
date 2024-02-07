f868c319ab16388ada0dc991e4aaee62432a616d
sowmyas123 <sowmyasiddu@gmail.com>
2024-02-07 16:25:04 +0530

    hello

---
+++ 03_05.Feb.2024/Lab016.py
@@ -0,0 +1,16 @@
+#None datatype means nothing
+#is not a default values
+#None is not zero
+#None is not an empty string
+#None is not same as False
+val = None
+print(val)
+print(type(val))
+
+#val2 = None+1 # is not possible
+#print(val2)
+
+string = "" #it will take memory space
+string = None #it will not take memory so None is used in python
+#while initialization None will not use memory
+string = null #in python there is no null only None
