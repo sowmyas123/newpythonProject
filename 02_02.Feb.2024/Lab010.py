998864e688aa4abf99dd106df7bf8bbbfdfa90ea
sowmyas123 <sowmyasiddu@gmail.com>
2024-02-07 16:19:15 +0530

    hello

---
+++ 02_02.Feb.2024/Lab010.py
@@ -0,0 +1,8 @@
+name = str(input("Enter your name:"))
+age = int(input("Enter your age:"))
+rollnumber = int(input("Enter your rollnumber:"))
+phonenumber = int(input("Enter your phonenumber:"))
+print("This is your name",name)
+print("This is your age", age)
+print("This is your rollnumber", rollnumber)
+print("This is your phonenumber", phonenumber)
