ce994dfea84c58922873df788b7bf93276acb7ea
sowmyas123 <sowmyasiddu@gmail.com>
2024-02-07 16:24:24 +0530

    hello

---
+++ 03_05.Feb.2024/Lab015.py
@@ -0,0 +1,15 @@
+name = "Sowmya"
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+
+#we can also do reverse - reverse indexation is possible
+name = "Sowmya"
+print(name[-3])
+
+name = "This is a big line"
+print(name[-1])
+print(len(name))
