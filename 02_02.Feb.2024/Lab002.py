e9cb32180bb6bb24daa66ea2f7ec5d58160a9844
sowmyas123 <sowmyasiddu@gmail.com>
2024-02-07 16:11:31 +0530

    hello

---
+++ 02_02.Feb.2024/Lab002.py
@@ -0,0 +1,27 @@
+#keywords and identifier
+#keyword are Reserved word, its already predefined
+#variable name = variable value
+#variable names are also called as identifiers starts with A-Z a-Z 0-9 and _
+
+age = 30
+print(age)
+age = "sowmya"
+print(age)
+age = 'C'
+print(age)
+
+#class = "sowmya" is not possible because class is a keyword
+#Class = "Sowmya" is possible because Class is not a keyword, python is case sensitive
+#123 = 596 is not possible because vairable name cannot start with numnbers
+#$ = "Sowmya" is not possible
+#_$ = "Sowmya" is not possible
+_123 = 596 #is possible variable name can start with underscore
+print(_123)
+
+_ = "siri"
+print(_)
+
+#to import all the keywords
+import keyword (command + enter)
+
+
