5aad496a6f7942e5f77534e069dd11ccc8e09c10
sowmyas123 <sowmyasiddu@gmail.com>
2024-02-07 16:12:53 +0530

    hello

---
+++ 02_02.Feb.2024/Lab003.py
@@ -0,0 +1,27 @@
+#integer
+age = 24
+rollnumber = 232121
+phonenumber = 98762302321
+
+#float
+pi = 3.14
+pi= -3.14
+gst = 2.8
+
+#Boolean
+isMarried = True
+areYouMarried = False
+
+#complex datatype
+i=iota = sq = 1
+a = 1+7j
+
+print(type(a))
+
+#Sequence
+name = "Sowmya"
+name2 = "Siri"
+char = 'C'
+print(type(name))
+print(type(name2))
+print(type(char))
