ba792a5f0bded7a17ec19deb8c17d0836da25f63
sowmyas123 <sowmyasiddu@gmail.com>
2024-02-07 16:25:51 +0530

    hello

---
+++ 03_05.Feb.2024/Lab017.py
@@ -0,0 +1,14 @@
+#Sequence - List data type
+#List is a collection of item
+#it is used with []
+#it can contain different data types
+my_shopping_list = ["fruits", "vegetables", "flowers", "groceries", "drinks"]
+print(len(my_shopping_list))
+print(my_shopping_list[0])
+print(my_shopping_list[3])
+print(my_shopping_list[-1])
+print(my_shopping_list[-5])
+
+list = [1,2,3, True, 9,10, "Sowmya"]
+print(type(list)) #type is data type <class 'list'>
+print(len(list))
