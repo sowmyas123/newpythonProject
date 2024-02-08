b31c58b2b62890d4a05afdbcb32a256ca0d8af61
sowmyas123 <sowmyasiddu@gmail.com>
2024-02-08 16:00:30 +0530

    hello

--- 04_07.Feb.2024/Lab022.py
+++ 04_07.Feb.2024/Lab022.py
@@ -1,3 +1,6 @@
+#Decimal
+age = 56
+
 #Binary number
 #we can store binary numbers in python
 #it is useful in marchine learning, AI, storing passwords
@@ -9,3 +12,20 @@ c = 0o130
 
 #Hexa number can be stored using 0x
 d = 0x12v
+
+#String
+name = "sowmya"
+name2 = 'sowmya'
+multi_line_string = """ This is a multi line
+stsring we can have long lines where
+you can type
+stories
+"""
+
+#Boolean literals
+x = True
+y = False
+
+# == is compare operator
+v1 = (1 == False)
+print(v1)
